2b22c5c721e0cc02357060bfaf114c874c410e41
EnesMehmet1 <81644782+EnesMehmet1@users.noreply.github.com>
2022-10-26 00:34:07 +0300

    java
    missing git

--- src/Employee.java
+++ src/Employee.java
@@ -1,25 +1,25 @@
 import java.sql.Date;
 
 public class Employee {
-	private String Name;
+	private String name;
 	private int salary;
 	private int workHours; 
-	private Date hireYear;
+	private int hireYear;
 	public Employee() {
 		super();
 	}
-	public Employee(String name, int salary, int workHours, Date hireYear) {
+	public Employee(String name, int salary, int workHours, int hireYear) {
 		super();
-		Name = name;
+		this.name = name;
 		this.salary = salary;
 		this.workHours = workHours;
 		this.hireYear = hireYear;
 	}
 	public String getName() {
-		return Name;
+		return name;
 	}
 	public void setName(String name) {
-		Name = name;
+		name = name;
 	}
 	public int getSalary() {
 		return salary;
@@ -33,10 +33,10 @@ public class Employee {
 	public void setWorkHours(int workHours) {
 		this.workHours = workHours;
 	}
-	public Date getHireYear() {
+	public int getHireYear() {
 		return hireYear;
 	}
-	public void setHireYear(Date hireYear) {
+	public void setHireYear(int hireYear) {
 		this.hireYear = hireYear;
 	}
 	
@@ -44,27 +44,53 @@ public class Employee {
 	
 	public float tax() {
 		if(salary<1000) {
-			return salary;
+			return 0;
 		}
 		else
 		{
-			salary+=salary*3/100;
-			return salary;
+			return salary*3/100;
 		}
 	}
 	
 	
-	public void bonus() {
+	public float bonus() {
+		int bonus=0;
 		if(workHours>40) {
-			return salary;
+			for(int i=40;i<workHours;i++) {
+				bonus+=30;
+			}
+			return bonus;
 		}
 		else
 		{
-			return salary;
+			return bonus;
 		}
 	}
 	
-	public void raiseSalary() {
-		
+	public float raiseSalary() {
+		if(hireYear+19<2021) {
+			return salary*15/100;
+		}
+		else if(hireYear+9<2021 && hireYear+20>2021) {
+			return salary*10/100;
+		}
+		else if(hireYear+10>2021) {
+			return salary*5/100;
+		}
+		else
+			return 0;
+	}
+	public void tostring() {
+		System.out.println("Adı : "+name);
+		System.out.println("Maaşı : "+salary);
+		System.out.println("çalışma saati : "+workHours);
+		System.out.println("Başlangıç yılı : "+hireYear);
+		System.out.println("Vergi : "+tax());
+		System.out.println("Bonus : "+bonus());
+		System.out.println("Maaş artışı : "+raiseSalary());
+		 float vbMaas=salary-tax()+bonus();
+		System.out.println("Vergiler ve Bnouslar ile birlikte maaş : "+vbMaas);
+		vbMaas=salary-tax()+bonus()+raiseSalary();
+		System.out.println("Toplam Maaş : "+vbMaas);
 	}
 }
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee employee=new Employee("Enes",15000,25,2023);
+		employee.tostring();
+	}
+
+}
